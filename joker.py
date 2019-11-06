0691352a9050828b1935837d5311ad4faec69dde
machchhindra01 <d3v1l9923@gmail.com>
2019-11-05 22:21:33 -0800

    issue

--- joker.py
+++ joker.py
@@ -4,3 +4,13 @@ num_two=int(input("Enter the second number: "))
 sum=num_one + num_two
 print(sum)
 #Sorry for my bad programming
+
+#Add two using def function
+def add_two (num_one,num_two):
+    return num_one + num_two
+num_one=int(input("Enter your first number: "))
+num_two=int(input("Enter the second number: "))
+print(add_two(num_one,num_two))
+
+#some issue
+#issue founds
