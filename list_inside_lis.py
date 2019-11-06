ade23a088cf260357990a9782f097b1cf14cf81a
machchhindra01 <d3v1l9923@gmail.com>
2019-11-06 01:28:52 -0800

    more

---
+++ list_inside_lis.py
@@ -0,0 +1,21 @@
+matrix=[[1,2,3],[4,5,6],[7,8,9]]
+#print(matrix[0])
+#print(matrix[2])
+
+#for loop
+
+#for i in matrix:
+#    print(i)
+
+#for sublist in matrix:
+#    for i in sublist:
+#        print(i)
+
+#print(matrix[1] [1])
+#print(matrix[2] [0])
+
+s="string"
+
+print(type(s))
+
+print(type(matrix))
---
+++ more_ab_list.py
@@ -0,0 +1,11 @@
+numbers=list(range(1,11))
+
+print(numbers)
+
+numbers.pop()
+
+print(numbers)
+
+print(numbers.index(1))
+
+print(numbers.index(5))
