40e90aa3f68245059ecb602eacdfcd0c4a7abfa3
machchhindra01 <d3v1l9923@gmail.com>
2019-11-07 07:52:11 -0800

    more add

---
+++ function_returning.py
@@ -0,0 +1,5 @@
+def func (int1, int2):
+    add=int1 + int2
+    mult=int1*int2
+    return add,mult
+print(func(1,3))
---
+++ tuple.py
@@ -0,0 +1,54 @@
+#mixed=(1,2,3,4.0)
+#print(type(mixed))
+#print(mixed)
+
+#for loop and tuple
+
+#for i in mixed:
+#    print(i)
+
+#tuple with one elememt
+
+#num=(1,)
+#print(type(num))
+
+#words=('words',)
+#mack=["word",]
+#jeet=(word)
+
+#print(type(words))
+#print(type(jeet))
+#print(type(mack))
+
+#tuple without paramiters
+
+#guitars="yamaha","baton rouge","taylor"
+#print(type(guitars))
+#print(guitars[1])
+
+#tuple unpacking
+
+#guitorist=("maneli jamel","eddie vander meer","andrew foy")
+#guitarist1, guitarist2, guitarist3=(guitorist)
+
+#print(guitarist1)
+#print(guitarist2)
+#print(guitarist3)
+
+#list inside tuple
+
+#favorites=("southern mannolia",["Tokyo Ghoul Theme","landscape"])
+#favorites[1].pop()
+#favorites[0].pop()
+#print(favorites)
+#favorites[1].append("we made it")
+#print(favorites)
+
+#min, max, sum
+
+#mixed=(1,2,3,4.0)
+mixed=[1,2,3,4]
+print(min(mixed))
+print(max(mixed))
+print(sum(mixed))
+
